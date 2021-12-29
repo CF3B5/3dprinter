81a1da7c85b6372605dfa0a75d3e22c1c0d489e0
CF3B5 <locke.cm@gmail.com>
2021-12-29 15:06:22 +0800

    更新小米蓝牙温度计，增加电量和异步进程逻辑

--- klipper/xiaomi_blue.py
+++ klipper/xiaomi_blue.py
@@ -7,6 +7,7 @@ import logging
 from bluepy import btle
 import struct
 from threading import Thread
+from multiprocessing import Process
 
 
 class XIAOMI_BLUE:
@@ -18,12 +19,14 @@ class XIAOMI_BLUE:
         self.reactor = self.printer.get_reactor()
         self.report_time = config.getint('report_time', 20, minval=10)
         self.mac = config.get('mac_address')
-        self.temp = self.min_temp = self.max_temp = self.humidity = 0.
+        self.temp = self.min_temp = self.max_temp = self.humidity = self.voltage = self.battery = 0.
         self.sample_timer = self.reactor.register_timer(self._sample_read)
         self.printer.add_object("xiaomi_blue " + self.name, self)
         self.printer.register_event_handler("klippy:connect",
                                             self.handle_connect)
-        self.bt = None
+        self.printer.register_event_handler("klippy:disconnect",
+                                            self.handle_disconnect)
+        self._thread = None
 
     def connect(self):
         if self._blue_connect is None:
@@ -33,10 +36,13 @@ class XIAOMI_BLUE:
     def close(self):
         if self._blue_connect is not None:
             self._blue_connect.disconnect()
-        self._blue_connect = None
+        self._blue_connect = self._thread = None
 
     def handle_connect(self):
         self.reactor.update_timer(self.sample_timer, self.reactor.NOW)
+
+    def handle_disconnect(self):
+        self.close()
 
     def setup_minmax(self, min_temp, max_temp):
         self.min_temp = min_temp
@@ -49,34 +55,44 @@ class XIAOMI_BLUE:
         return self.report_time
 
     def _sample_read(self, eventtime):
-        try:
-            self.bt = XiaoMiTempBt(self)
-            self.bt.start()
-        except Exception:
-            logging.exception('[xiaomi] error')
-
         # try:
-        #     logging.info('[xiaomi] read data')
-        #     p = self.connect()
-        #     p.writeCharacteristic(0x0038, b'\x01\x00', True)
-        #     p.writeCharacteristic(0x0046, b'\xf4\x01\x00', True)
-        #     measure = Measure(self)
-        #     p.withDelegate(measure)
-        #
-        #     # if p.waitForNotifications(3000):
-        #     #     logging.info('[xiaomi] read timeout!')
-        #     # self.close()
-        #
-        # except Exception:
-        #     logging.exception("[xiaomi] read data error")
-        #     # self.temp = self.humidity = .0
+        #     if self._thread is None or not self._thread.isAlive():
+        #         self._thread = XiaoMiTempBt(self)
+        #         self._thread.start()
+        #     else:
+        #         logging.info('xiaomi: thread is alive!')
+        # except Exception as e:
+        #     logging.info("xiaomi error: %s" % e.message)
         #     self.close()
-        #     # return self.reactor.NEVER
+        try:
+            if self._thread is None or not self._thread.is_alive():
+                self._thread = Process(target=self.read())
+                self._thread.start()
+            else:
+                logging.info('xiaomi: thread is alive!')
+        except Exception as e:
+            logging.info("xiaomi error: %s" % e.message)
 
         mcu = self.printer.lookup_object('mcu')
         measured_time = self.reactor.monotonic()
         self._callback(mcu.estimated_print_time(measured_time), self.temp)
         return measured_time + self.report_time
+
+    def read(self):
+        try:
+            p = self.connect()
+            p.writeCharacteristic(0x0038, b'\x01\x00', True)
+            p.writeCharacteristic(0x0046, b'\xf4\x01\x00', True)
+            measure = Measure(self)
+            p.withDelegate(measure)
+
+            if not p.waitForNotifications(1000):
+                logging.info('xiaomi: read timeout!')
+            self.close()
+
+        except Exception as e:
+            logging.info("xiaomi error: %s" % e.message)
+            self.close()
 
     def get_temp(self, eventtime):
         return self.temp, 0.
@@ -84,7 +100,9 @@ class XIAOMI_BLUE:
     def get_status(self, eventtime):
         return {
             'temperature': round(self.temp, 2),
-            'humidity': self.humidity
+            'humidity': self.humidity,
+            'voltage': self.voltage,
+            'battery': self.battery
         }
 
 
@@ -95,22 +113,19 @@ class XiaoMiTempBt(Thread):
 
     def run(self):
         try:
-            logging.info('[xiaomi] read data')
             p = self.obj.connect()
             p.writeCharacteristic(0x0038, b'\x01\x00', True)
             p.writeCharacteristic(0x0046, b'\xf4\x01\x00', True)
             measure = Measure(self.obj)
             p.withDelegate(measure)
 
-            if not p.waitForNotifications(300):
-                logging.info('[xiaomi] read timeout!')
-            # self.close()
-
-        except Exception:
-            logging.exception("[xiaomi] read data error")
-            # self.temp = self.humidity = .0
+            if not p.waitForNotifications(1000):
+                logging.info('xiaomi: read timeout!')
             self.obj.close()
-            # return self.reactor.NEVER
+
+        except Exception as e:
+            logging.info("xiaomi error: %s" % e.message)
+            self.obj.close()
 
 
 class Measure(btle.DefaultDelegate):
@@ -132,11 +147,17 @@ class Measure(btle.DefaultDelegate):
             humidity = int(data[2:3].encode('hex'), 16)
             if humidity > 0:
                 self.obj.humidity = humidity
-            logging.error("xiaomi_blue_code: ", data.encode('hex'), self.obj.temp, self.obj.humidity)
-            # self.voltage = int(data[3:5].encode('hex'), 16) / 1000
-            # self.battery = round((self.voltage - 2) / (3.261 - 2) * 100, 2)
+            v = data[3:5].encode('hex').decode('hex')
+            voltage = float(struct.unpack('<h', v)[0]) / 1000
+            if voltage > 0:
+                self.obj.voltage = voltage
+            battery = round((voltage - 2) / (3.261 - 2) * 100, 2)
+            if battery > 0:
+                self.obj.battery = battery
+            logging.info("xiaomi: temp=%f, humidity=%f, voltage=%f, battery=%f" % (temp, humidity, voltage, battery))
+
         except Exception as e:
-            logging.error('xiaomi_blue_error', e)
+            logging.info("xiaomi error: %s" % e.message)
 
 
 def load_config(config):
